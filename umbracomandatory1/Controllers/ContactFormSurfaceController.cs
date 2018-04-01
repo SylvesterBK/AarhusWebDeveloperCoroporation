36d03af2c2e8e9e5977f71779ae70775ba2cc82c
Sylvester <contact@sylvesterbk.dk>
2018-04-01 17:23:46 +0200

    tættere på contactform

--- umbracomandatory1/Controllers/ContactFormSurfaceController.cs
+++ umbracomandatory1/Controllers/ContactFormSurfaceController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 using umbracomandatory1.ViewModels;
 using Umbraco.Core.Models;
 using Umbraco.Web.Mvc;
+using System.Net.Mail;
+using umbracomandatory1.Controllers;
+
 
 namespace umbracomandatory1.Controllers
 {
@@ -14,9 +17,40 @@ namespace umbracomandatory1.Controllers
         // GET: ContactFormSurface
         public ActionResult Index()
         {
+
+            return PartialView("ContactForm", new ContactForm());
+
+        }    
             
-            TempData["success"] = true;
-            return PartialView("ContactForm", new ViewModels.ContactForm());
+            [HttpPost]
+            public ActionResult HandleFormSubmit(ContactForm model)
+             {
+            if (!ModelState.IsValid) { return CurrentUmbracoPage(); }
+            else
+            {
+                MailMessage message = new MailMessage();
+                message.To.Add("eaasykj@students.eaaa.dk");
+                message.Subject = model.Subject;
+                message.From = new MailAddress(model.Email, model.Name);
+                message.Body = model.Message;
+
+
+                IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "Message");
+                comment.SetValue("messageName", model.Name);
+                comment.SetValue("email", model.Email);
+                comment.SetValue("subject", model.Subject);
+                comment.SetValue("messageContent", model.Message);
+                Services.ContentService.Save(comment);
+
+
+                //Services.ContentService.SaveAndPublishWithStatus(comment);
+                
+
+
+                TempData["success"] = true;
+                return RedirectToCurrentUmbracoPage();
+            }
+        }
         }
     }
 }
--- umbracomandatory1/ViewModels/ContactForm.cs
+++ umbracomandatory1/ViewModels/ContactForm.cs
@@ -4,18 +4,19 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using umbracomandatory1.Controllers;
+using umbracomandatory1.ViewModels;
 
 namespace umbracomandatory1.ViewModels
 {
     public class ContactForm
     {
-        //commentthirdbranch
+        //commentthirdbranchkuugkugkhku
         [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Please enter your Email")]
         [Display(Name = "Email")]
-        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+/@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$", 
-            ErrorMessage = "Please enter a valid email address")]
+        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+/@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$", ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Please enter your subject")]
         public string Subject { get; set; }
